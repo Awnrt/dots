ae3cc535a660b104a4587c9c09f662454eb7faa7
Awnrt <roman.levikoff@gmail.com>
2024-03-29 19:06:06 +0300

    update

--- rice.sh
+++ rice.sh
@@ -9,5 +9,33 @@ cp -r kitty /home/$USER/.config
 cp -r nvim /home/$USER/.config
 cp -r waybar /home/$USER/.config
 cp -r wofi /home/$USER/.config
+cp -r .papes /home/$USER
 
+echo "options hid_apple fnmode=0" > /etc/modprobe.d/hid_apple.conf 
+echo "options nvidia NVreg_RegistryDwords=\"PowerMizerEnable=0x1; PerfLevelSrc=0x2222; PowerMizerLevel=0x3; PowerMizerDefault=0x3; PowerMizerDefaultAC=0x3\"" > /etc/modprobe.d/nvidia.conf
 
+sudo mkdir /etc/systemd/system/dhcpcd@eno1.service.d/
+sudo echo "[Service]" >> /etc/systemd/system/dhcpcd@eno1.service.d/no-wait.conf
+sudo echo "ExecStart=" >> /etc/systemd/system/dhcpcd@eno1.service.d/no-wait.conf
+sudo echo "ExecStart=/usr/bin/dhcpcd -b -q %I" >> /etc/systemd/system/dhcpcd@eno1.service.d/no-wait.conf
+
+sudo echo "noarp" >> /etc/dhcpcd.conf
+
+sudo systemctl enable cpupower.service
+sudo systemctl start cpupower.service
+
+sudo echo "governor='performance'" >> /etc/default/cpupower
+
+sudo echo "min_freq='4.3GHz'" >> /etc/default/cpupower
+
+sudo echo "min_freq='4.8GHz'" >> /etc/default/cpupower
+
+# Uncomment these lines to have yay automatically installed
+#
+#cd /home/$USER
+#wget https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz
+#tar -xzf yay.tar.gz
+#cd yay
+#makepkg -csi
+
+sudo mkinitcpio -P
