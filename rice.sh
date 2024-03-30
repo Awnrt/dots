300fa18c437ded219cb44a9ae1cc5502a7813d9e
Awnrt <roman.levikoff@gmail.com>
2024-03-30 23:21:29 +0300

    added some stuff

--- rice.sh
+++ rice.sh
@@ -11,12 +11,6 @@ DEPLIST="`sed -e 's/#.*$//' -e '/^$/d' dependencies.txt | tr '\n' ' '`"
 
 sudo pacman -Sq $DEPLIST --noconfirm
 
-#wget https://github.com/dracula/gtk/archive/master.zip
-#unzip master.zip
-
-#wget https://github.com/dracula/gtk/files/5214870/Dracula.zip
-#unzip Dracula.zip
-
 wget https://github.com/dracula/gtk/releases/download/v4.0.0/Dracula-cursors.tar.xz
 tar -xf Dracula-cursors.tar.xz 
 
@@ -24,17 +18,8 @@ mkdir /home/$USER/.local
 mkdir /home/$USER/.local/share
 mkdir /home/$USER/.local/share/themes
 mkdir /home/$USER/.local/share/icons
-cp -r gtk-master /home/$USER/.local/share/themes/Dracula
-cp -r Dracula /home/$USER/.local/share/icons
 cp -r Dracula-cursors /home/$USER/.local/share/icons
 cp -r papes /home/$USER/.local/share
-# Uncomment these lines to have yay automatically installed
-#
-#cd /home/$USER
-#wget https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz
-#tar -xzf yay.tar.gz
-#cd yay
-#makepkg -csi
 
 gsettings set org.gnome.desktop.interface font-name 'JetBrainsMonoNL NF Bold Mono 11'
 gsettings set org.gnome.desktop.interface monospace-font-name 'JetBrainsMonoNL NF Bold Mono 11'
