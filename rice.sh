859b9d9f5857812b5ecc841b75b96f7297bf5da9
Awnrt <roman.levikoff@gmail.com>
2024-03-30 23:18:07 +0300

    added some stuff

--- rice.sh
+++ rice.sh
@@ -5,17 +5,17 @@ cp -r kitty /home/$USER/.config
 cp -r nvim /home/$USER/.config
 cp -r waybar /home/$USER/.config
 cp -r wofi /home/$USER/.config
-cp -r .papes /home/$USER
+
 
 DEPLIST="`sed -e 's/#.*$//' -e '/^$/d' dependencies.txt | tr '\n' ' '`"
 
 sudo pacman -Sq $DEPLIST --noconfirm
 
-wget https://github.com/dracula/gtk/archive/master.zip
-unzip master.zip
+#wget https://github.com/dracula/gtk/archive/master.zip
+#unzip master.zip
 
-wget https://github.com/dracula/gtk/files/5214870/Dracula.zip
-unzip Dracula.zip
+#wget https://github.com/dracula/gtk/files/5214870/Dracula.zip
+#unzip Dracula.zip
 
 wget https://github.com/dracula/gtk/releases/download/v4.0.0/Dracula-cursors.tar.xz
 tar -xf Dracula-cursors.tar.xz 
@@ -27,7 +27,7 @@ mkdir /home/$USER/.local/share/icons
 cp -r gtk-master /home/$USER/.local/share/themes/Dracula
 cp -r Dracula /home/$USER/.local/share/icons
 cp -r Dracula-cursors /home/$USER/.local/share/icons
-
+cp -r papes /home/$USER/.local/share
 # Uncomment these lines to have yay automatically installed
 #
 #cd /home/$USER
@@ -38,9 +38,9 @@ cp -r Dracula-cursors /home/$USER/.local/share/icons
 
 gsettings set org.gnome.desktop.interface font-name 'JetBrainsMonoNL NF Bold Mono 11'
 gsettings set org.gnome.desktop.interface monospace-font-name 'JetBrainsMonoNL NF Bold Mono 11'
-gsettings set org.gnome.desktop.interface gtk-theme "Dracula"
-gsettings set org.gnome.desktop.wm.preferences theme "Dracula"
-gsettings set org.gnome.desktop.interface icon-theme "Dracula"
+gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
+gsettings set org.gnome.desktop.wm.preferences theme "Adwaita-dark"
+gsettings set org.gnome.desktop.interface icon-theme "Adwaita-dark"
 gsettings set org.gnome.desktop.interface cursor-theme "Dracula-cursors"
 
 chsh -s $(which fish)
