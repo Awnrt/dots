4215b7570b48b818b57cff4906f994c0e540b388
Awnrt <roman.levikoff@gmail.com>
2024-04-02 23:20:36 +0300

    dif themes

--- fish/config.fish
+++
@@ -1,4 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    [ "$(tty)" = "/dev/tty1" ] && ! pidof -s Hyprland >/dev/null 2>&1 && exec "/home/awy/.config/hypr/start.sh"
-end
